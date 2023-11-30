5c7a4bc8c59106f5889c4c7aabb3e8babf260188
ivozhelev <141124208+ivozhelev@users.noreply.github.com>
2023-11-30 16:51:49 +0200

    Added fixture data to registration tests (#2)

--- cypress/tests/e2e/users/registration.cy.js
+++ cypress/tests/e2e/users/registration.cy.js
@@ -1,20 +1,24 @@
+const registrationTestData = require('../../../fixtures/e2e/users/registration.json');
+
 describe('Visit SpaceFortuna Home Page and verify label existence', () => {
   const testConfiguration = {};
 
   before(() => {
     cy.fixture('e2e/environment.json').then((environment) => {
-      testConfiguration.host = environment.hosts.wallet;
-    });
-
-    cy.fixture('api/wallet/bonus-history.fixture.json').then((bonusHistoryTestData) => {
-      testConfiguration.bonusHistoryTestData = bonusHistoryTestData;
+      testConfiguration.host = environment.hosts.frontoffice;
     });
   });
 
-  it('passes 1', () => {
-    cy.viewport(1920, 1080);
-    cy.visit('https://frontoffice.dev.inovadatabv.com/');
-    cy.wait(5000);
-    cy.get('body').should('exist');
-  });
+  Object.keys(registrationTestData.invalidPayloads).forEach(key => {
+    const invalidTestPayload = registrationTestData.invalidPayloads[key];
+
+    it('Registration with invalid test data on first step should not proceed further', () => {
+      cy.viewport(1920, 1080);
+      cy.visit(testConfiguration.host);
+      cy.get('#header-sign-up-btn').click();
+      cy.get('#email').clear().type(invalidTestPayload.email);
+      cy.get('#password').clear().type(invalidTestPayload.password);
+      cy.get('#registration-next-step-btn').should('be.disabled');
+    });
+  })
 })
