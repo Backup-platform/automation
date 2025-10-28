1078cf07ab8583f08464f128fa46a8414da5d457
Lora Makedonska <lmakedonska@sbtsolution.com>
2025-10-28 12:03:55 +0200

    Adding fixtures to carousel

--- apps/grandzbet-e2e/pages/landingPage/carousel.po.ts
+++ apps/grandzbet-e2e/pages/landingPage/carousel.po.ts
@@ -1,19 +1,15 @@
-import { Page, expect } from '@playwright/test';
+import { Page } from '@playwright/test';
 import { step } from '@test-utils/decorators';
-import { assertVisible, assertEnabled } from '@test-utils/assertions';
+import { assertVisible } from '@test-utils/assertions';
 import { compositeLocator } from '@test-utils/core-types';
-
-
 
 export class LandingPageCarousel {
     readonly page: Page;
-
     constructor(page: Page) {
         this.page = page;
     }
 
     // ---locators ---
-
     private readonly container = compositeLocator(() => this.page.locator('section#home-hero').first(), 'Carousel container');
     private readonly image = compositeLocator(() => this.page.locator('#home-hero img').first(), 'Carousel Image');
     private readonly title = compositeLocator(() => this.page.locator('p.font-roboto.text-2xl.font-bold').first(), 'Carousel title');
@@ -21,34 +17,18 @@ export class LandingPageCarousel {
     private readonly tag = compositeLocator(() => this.page.locator('#tag').first(), 'Carousel tag');
     private readonly dots = compositeLocator(() => this.page.locator('div.relative.flex-row.gap-2').first(), 'Carousel dots');
     private readonly activeDot = compositeLocator(() => this.page.locator('div.flex-row.gap-2 button.bg-primary').first(), 'Carousel active dot');
-     private readonly ctaButton = compositeLocator(() => this.page.locator('#home-hero button').first(),'Carousel cta button');
-
-
+    private readonly ctaButton = compositeLocator(() => this.page.locator('#home-hero button').first(), 'Carousel cta button');
 
     // --- assertions ---
-
-
     @step('Carousel: Elements visibility')
-
     public async validateElementsVisible(softAssert = false): Promise<void> {
-
         await assertVisible(this.container, softAssert);
-
         await assertVisible(this.image, softAssert);
-
         await assertVisible(this.title, softAssert);
-
         await assertVisible(this.subtitle, softAssert);
-
         await assertVisible(this.tag, softAssert);
-
         await assertVisible(this.dots, softAssert);
-
         await assertVisible(this.activeDot, softAssert);
-
         await assertVisible(this.ctaButton, softAssert);
-
     }
-
-
 }
--- apps/grandzbet-e2e/tests/regression/desktop/homepage.spec.ts
+++ apps/grandzbet-e2e/tests/regression/desktop/homepage.spec.ts
@@ -1,38 +1,24 @@
 import test from '../../../pages/base/base.po';
 import path from 'path';
-import { LandingPageCarousel } from '../../../pages/landingPage/carousel.po';
-
 
 
 test.beforeEach(async ({ page }) => {
   await page.goto(`${process.env.URL}`, { waitUntil: 'domcontentloaded' });
 });
 
-
 test.describe("Landing page- Carousel elements", () => {
   test.use({ storageState: path.resolve(__dirname, '../../../playwright/.auth/noAuthentication.json') });
 
-
-  test('Guest: validate carousel elements visibility', async ({ page }) => {
-    const carousel = new LandingPageCarousel(page);
-
-    await carousel.validateElementsVisible();
+  test('Guest: validate carousel elements visibility', async ({ landingPageCarousel }) => {
+    await landingPageCarousel.validateElementsVisible();
   });
 
-
-  test('Member: validate carousel elements visibility', async ({ loginPage, menuItems, page }) => {
-
-
+  test('Member: validate carousel elements visibility', async ({ loginPage, menuItems, page, landingPageCarousel }) => {
     await menuItems.clickLogin();
     await loginPage.validatePageElementsVisible();
     await loginPage.actionLogin(`${process.env.USER}`, `${process.env.PASS}`);
     await page.waitForURL(`${process.env.URL}`, { waitUntil: 'domcontentloaded' });
     await menuItems.validateUserItems();
-
-    const carousel = new LandingPageCarousel(page);
-
-    await carousel.validateElementsVisible();
-
+    await landingPageCarousel.validateElementsVisible();
   });
-
 });
